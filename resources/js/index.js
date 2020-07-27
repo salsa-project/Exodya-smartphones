150c95f2a102b6422a298c8086331f4d863a665a
salsa-project <intenso524@gmail.com>
2020-07-27 23:55:58 +0100

    [ADD][Inprogress] slider ..

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,6 +1,10 @@
 const mobileMenuIcon = document.getElementById('menu-btn');
 const navLayer = document.getElementsByClassName('nav-layer')[0];
 const navList = document.getElementById('navbar-list');
+const sectionTopItems = document.getElementsByClassName('section-top-item');
+const sectionTopSlide = document.getElementById("section-top-items-slide")
+const sliderArrowL = document.getElementsByClassName('left-arrow-slider')[0];
+const sliderArrowR = document.getElementsByClassName('right-arrow-slider')[0];
 const acceuil = document.getElementsByClassName('navbar-list-item')[1];
 const lesMarques = document.getElementsByClassName('navbar-list-item')[2];
 const lesDernies = document.getElementsByClassName('navbar-list-item')[3];
@@ -9,6 +13,42 @@ const similar = document.getElementsByClassName('navbar-list-item')[5];
 const rechercheLayer = document.getElementsByClassName('search-bar-options-layer')[0];
 const rechercheFilterBtn = document.getElementById("filters-btn");
 const rechercheBox = document.getElementsByClassName('search-bar-options')[0];
+
+
+
+// display/hide recherche avancé
+rechercheFilterBtn.addEventListener('click', function(){
+  rechercheLayer.classList.toggle('search-bar-options-layer-visible');
+})
+window.addEventListener('click', function(e){
+  if(rechercheLayer.contains(e.target) && !rechercheBox.contains(e.target)){
+    rechercheLayer.classList.toggle('search-bar-options-layer-visible')
+  }
+})
+
+// toggle mobile menu
+mobileMenuIcon.addEventListener('click', function(){
+  navLayer.classList.toggle('nav-layer-hide');
+  (navLayer.classList[1] !== 'nav-layer-hide') ? navList.style.transform = 'translateX(0%)' : navList.style.transform = 'translateX(-100%)';
+})
+
+// slider mobile
+// @ TODO: fix layout pc version
+// @ this loop is causing problem of layout in the pc version
+let itemPos = 0;
+Array.from(sectionTopItems).forEach(item => {
+  item.style = `transform: translateX(${itemPos}px)`;
+  itemPos += item.offsetWidth + 200;
+});
+
+//
+let fullWidth = 0;
+sliderArrowL.addEventListener('click', function(){
+  fullWidth += sectionTopItems[0].offsetWidth + 200;
+  sectionTopSlide.style = `transform: translateX(${-fullWidth}px);`;
+})
+
+
 
 
 // Fixed navbar
@@ -20,6 +60,18 @@ const rechercheBox = document.getElementsByClassName('search-bar-options')[0];
 //       navbar.classList.remove('navbar-list-fixed');
 //     }
 // });
+
+// hide mobile menu when layer clicked
+// window.addEventListener('click', function(e){
+//   if(navLayer.contains(e.target) && !navList.contains(e.target) ){
+    
+//       navList.style.transform = 'translateX(-100%)';
+//     setTimeout(function(){
+//       navLayer.classList.toggle('nav-layer-hide');
+//       mobileMenuIcon.checked = false;
+//     }, 200)
+//   }
+// })
 
 
 
@@ -36,37 +88,3 @@ switch(window.location.pathname){
   lesDernies.classList.add('navbar-item-active');
   break;
 }
-
-// display/hide recherche avancé
-rechercheFilterBtn.addEventListener('click', function(){
-  rechercheLayer.classList.toggle('search-bar-options-layer-visible');
-})
-window.addEventListener('click', function(e){
-  if(rechercheLayer.contains(e.target) && !rechercheBox.contains(e.target)){
-    rechercheLayer.classList.toggle('search-bar-options-layer-visible')
-  }
-})
-
-// toggle mobile menu
-mobileMenuIcon.addEventListener('click', function(){
-  navLayer.classList.toggle('nav-layer-hide');
-  setTimeout(function(){
-    if(navLayer.classList[1] !== 'nav-layer-hide'){
-      navList.style.transform = 'translateX(0%)';
-    }else{
-      navList.style.transform = 'translateX(-100%)';
-    }
-    }, 100)
-})
-
-//
-window.addEventListener('click', function(e){
-  if(navLayer.contains(e.target) && !navList.contains(e.target) ){
-    
-      navList.style.transform = 'translateX(-100%)';
-    setTimeout(function(){
-      navLayer.classList.toggle('nav-layer-hide');
-      mobileMenuIcon.checked = false;
-    }, 300)
-  }
-})
