5a79ac3ef272da7845a36589b092596555d6ec55
salsa-project <intenso524@gmail.com>
2020-07-28 23:49:26 +0100

    [Inprogress][slider] (les derniers) ..

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,8 +3,13 @@ const navLayer = document.getElementsByClassName('nav-layer')[0];
 const navList = document.getElementById('navbar-list');
 const sectionTopItems = document.getElementsByClassName('section-top-item');
 const sectionTopSlide = document.getElementById("section-top-items-slide")
-const sliderArrowL = document.getElementsByClassName('left-arrow-slider')[0];
-const sliderArrowR = document.getElementsByClassName('right-arrow-slider')[0];
+const sectionDernierSlide = document.getElementsByClassName("section-derniers-items-container")[0];
+const sectionDernierItems = document.getElementsByClassName("section-derniers-item");
+const topSliderArrowL = document.getElementsByClassName('left-arrow-slider')[0];
+const topSliderArrowR = document.getElementsByClassName('right-arrow-slider')[0];
+const dernierSliderArrowL = document.getElementsByClassName('dernier-left-arrow')[0];
+const dernierSliderArrowR = document.getElementsByClassName('dernier-right-arrow')[0];
+
 const acceuil = document.getElementsByClassName('navbar-list-item')[1];
 const lesMarques = document.getElementsByClassName('navbar-list-item')[2];
 const lesDernies = document.getElementsByClassName('navbar-list-item')[3];
@@ -26,37 +31,63 @@ window.addEventListener('click', function(e){
   }
 })
 
-// toggle mobile menu
-mobileMenuIcon.addEventListener('click', function(){
-  navLayer.classList.toggle('nav-layer-hide');
-  (navLayer.classList[1] !== 'nav-layer-hide') ? navList.style.transform = 'translateX(0%)' : navList.style.transform = 'translateX(-100%)';
-})
+// @ If device is mobile then ..
+// @ to prevent effecting pc version
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (isMobile) {
+  /****************@ start isMobile*****************/
+  // toggle mobile menu
+  mobileMenuIcon.addEventListener('click', function(){
+    navLayer.classList.toggle('nav-layer-hide');
+    (navLayer.classList[1] !== 'nav-layer-hide') ? navList.style.transform = 'translateX(0%)' : navList.style.transform = 'translateX(-100%)';
+  })
+  /**************************
+        slider mobile
+  **************************/
+  // top marques
+  let itemPos = 0;
+  Array.from(sectionTopItems).forEach(item => {
+    item.style = `transform: translateX(${itemPos}px)`;
+    itemPos += item.offsetWidth + 200;
+  });
 
-// slider mobile
-// @ TODO: fix layout pc version
-// @ this loop is causing problem of layout in the pc version
-let itemPos = 0;
-Array.from(sectionTopItems).forEach(item => {
-  item.style = `transform: translateX(${itemPos}px)`;
-  itemPos += item.offsetWidth + 200;
-});
+  // slider L/R triggers
+  let slideLimit = 0;
+  let fullWidth = 0;
+  topSliderArrowR.addEventListener('click', function(){
+    if(slideLimit == 4) return;
+    fullWidth += sectionTopItems[0].offsetWidth + 200;
+    sectionTopSlide.style = `transform: translateX(${-fullWidth}px);`;
+    slideLimit++;
+  })
+  topSliderArrowL.addEventListener('click', function(){
+    if(slideLimit == 0) return;
+    fullWidth -= sectionTopItems[0].offsetWidth + 200;
+    sectionTopSlide.style = `transform: translateX(${-fullWidth}px);`;
+    slideLimit--;
+  })
 
-// slider L/R triggers
-let slideLimit = 0;
-let fullWidth = 0;
-sliderArrowR.addEventListener('click', function(){
-  if(slideLimit == 4) return;
-  fullWidth += sectionTopItems[0].offsetWidth + 200;
-  sectionTopSlide.style = `transform: translateX(${-fullWidth}px);`;
-  slideLimit++;
-})
-sliderArrowL.addEventListener('click', function(){
-  if(slideLimit == 0) return;
-  fullWidth -= sectionTopItems[0].offsetWidth + 200;
-  sectionTopSlide.style = `transform: translateX(${-fullWidth}px);`;
-  slideLimit--;
-})
+  // les derniers
+  let itemDerniersPos = 0;
+  Array.from(sectionDernierItems).forEach(item => {
+    item.style = `transform: translateX(${itemDerniersPos}px)`;
+    itemDerniersPos += item.offsetWidth + 200;
+  });
+  dernierSliderArrowR.addEventListener('click', function(){
+    if(slideLimit == 2) return;
+    fullWidth += sectionDernierItems[0].offsetWidth + 200;
+    sectionDernierSlide.style = `transform: translateX(${-fullWidth}px);`;
+    slideLimit++;
+  })
+  dernierSliderArrowL.addEventListener('click', function(){
+    if(slideLimit == 0) return;
+    fullWidth -= sectionDernierItems[0].offsetWidth + 200;
+    sectionDernierSlide.style = `transform: translateX(${-fullWidth}px);`;
+    slideLimit--;
+  })
 
+  /****************@ END isMobile*****************/
+}
 
 
 // Fixed navbar
